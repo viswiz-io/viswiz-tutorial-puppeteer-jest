61759e0887825df0ba5cfdf7c3c1a258ff70f608
Valentin Agachi <github-com@agachi.name>
2018-02-22 21:52:12 +0100

    Start and stop Puppeteer during tests :space_invader:

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	globalSetup: './tests/setup.js',
 	globalTeardown: './tests/teardown.js',
+	testEnvironment: './tests/test-environment.js',
 	testMatch: ['**/tests/**/*-test.js'],
 };
--- tests/teardown.js
+++ tests/teardown.js
@@ -1,3 +1,5 @@
 module.exports = async function() {
 	global.__SERVER__.close();
+
+	await global.__BROWSER__.close();
 };
